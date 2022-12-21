cce57391ae7f4369b65cf6c4203f22fea1c3de9b
Степан <www.niharaktera@yandex.ru>
2022-12-21 16:32:32 +0300

    2 домашка по ООП (Инкапсуляция)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
+Car car1 = new Car() {};
+        System.out.println(car1);
+        car1.setKey(car1.tt);
+        System.out.println(car1);
     }
 }
