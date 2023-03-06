076d7a67ed527988e56c94cf37428698ec804ef7
Степан <www.niharaktera@yandex.ru>
2023-03-06 22:31:37 +0300

    задача на функциональное программирование (курсовая)

--- src/Main.java
+++ src/Main.java
@@ -9,36 +9,12 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
+    //String string = "Топ-10 самых часто упоминаемых слов, упорядоченных по количеству упоминаний в обратном порядке. В случае одинакового количества упоминаний слова должны быть отсортированы по алфавиту."; ServiceClass.operate(string);
+    // оставил для быстрой проверки
     public static void main(String[] args) {
-        Service.trying();
-        int chose = 0;
-        for (; ; ) {
-            for (; ; ) {
-                System.out.println("Выберите дейсвтвие?\n1 - создать новую задачу\n2 - изменить задачу\n3 - искать задачу\n4 - удалить задачу\n0 - выйти");
-                try {
-                    chose = new Scanner(System.in).nextInt();
-                    break;
-                } catch (NumberFormatException ignored) {
-                }
-            }
-            switch (chose) {
-                case 0:
-                    System.out.println("До свидания");
-                    return;
-                case 1:
-                    Service.createTask();
-                    break;
-                case 2:
-                    Change.ChangeTask();
-                    break;
-                case 3:
-                    Search.searchTask();
-                    break;
-                case 4:
-                    Service.deleteTask();
-                    break;
-            }
-        }
+        for (;;) {
+            ServiceClass.operate();}
+    }
 
 
     }
